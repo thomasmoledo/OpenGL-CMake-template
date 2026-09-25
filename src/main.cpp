ea41b3c093b97f9d00abd4557c4146b8f8019436
Thomas Moledo <77012252+thomasmoledo@users.noreply.github.com>
2026-09-25 09:18:30 -0400

    Lecture 2 Update
    Added ShaderProgram and other 4 shader dependacy files. Updated main.cpp for lecture 2. Updated CMakeLists.txt to include new files

--- src/main.cpp
+++ src/main.cpp
@@ -1,88 +1,157 @@
 /**
  * @file main.cpp
- * @brief A simple program that creates a window with a green background.
+ * @brief A simple program that creates a window with OpenGL context and
+ * renders a colored triangle. This program demonstrates basic usage of SDL to
+ * create a window, create an OpenGL context, and render a simple colored
+ * triangle using shaders.
  * @date 2026-09-25
  */
+#define GL_SILENCE_DEPRECATION
+#define GL_GLEXT_PROTOTYPES 1
 
-#include <glad/gl.h>
-#include <SDL3/SDL.h>
-#include <SDL3_image/SDL_image.h>
-#include <SDL3_mixer/SDL_mixer.h>
-#include <iostream>
+#include "ShaderProgram.h"               // We'll talk about these later in the course
+#include "glm/gtc/matrix_transform.hpp"  // Matrix transformation methods
 
-/**
- Note here that `constexpr` is basically a compile-time version of `const`.  They do the same thing, for our purposes,
- tho.
- */
- // The size of our literal game window
+
+enum AppStatus { RUNNING, TERMINATED };
+
+// Our window dimensions
 constexpr int WINDOW_WIDTH = 640 * 2,
 WINDOW_HEIGHT = 480 * 2;
 
-// The background colours may change in the course of our games, so they can also be
-// variables
+// Background color components
 constexpr float BG_RED = 0.1922f,
 BG_BLUE = 0.549f,
 BG_GREEN = 0.9059f,
 BG_OPACITY = 1.0f;
 
-// it's good practice to add a `g_` to global variables' names
-SDL_Window* g_display_window = nullptr;
-bool g_game_is_running = true;
+// Our viewport—or our "camera"'s—position and dimensions
+constexpr int VIEWPORT_X = 0,
+VIEWPORT_Y = 0,
+VIEWPORT_WIDTH = WINDOW_WIDTH,
+VIEWPORT_HEIGHT = WINDOW_HEIGHT;
 
-int main(int argc, char* argv[])
+// Our shader filepaths; these are necessary for a number of things
+// Not least, to actually draw our shapes 
+// We'll have a whole lecture on these later
+constexpr char V_SHADER_PATH[] = "shaders/vertex.glsl",
+F_SHADER_PATH[] = "shaders/fragment.glsl";
+
+// Our object's fill colour
+constexpr float TRIANGLE_RED = 1.0,
+TRIANGLE_BLUE = 0.4,
+TRIANGLE_GREEN = 0.4,
+TRIANGLE_OPACITY = 1.0;
+
+constexpr float HARDCODED_DELTA_TIME = 0.0166f;
+
+AppStatus g_app_status = RUNNING;
+SDL_Window* g_display_window;
+
+ShaderProgram g_shader_program;
+
+glm::mat4 g_view_matrix, // Defines the position (location and orientation) of the camera
+g_model_matrix,          // Defines every translation, rotation, and/or scaling applied to an object; we'll look at these next week
+g_projection_matrix;     // Defines the characteristics of your camera, such as clip panes, field of view, projection method, etc.
+
+
+void initialise()
 {
-    // Initialising
+    // HARD INITIALISE ———————————————————————————————————————————————————————————————————
     SDL_Init(SDL_INIT_VIDEO);
+    g_display_window = SDL_CreateWindow("Hello, Triangle!",
+        WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
 
-    g_display_window = SDL_CreateWindow("Hello, World!",
-        WINDOW_WIDTH,WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
-
-
-    // If the window could not be created, then we should quit the program
     if (g_display_window == nullptr)
     {
-        std::cerr << "Error: SDL window could not be created.\n";
+        std::cerr << "ERROR: SDL Window could not be created.\n";
+        g_app_status = TERMINATED;
+
         SDL_Quit();
-        return 1;
+        exit(1);
     }
 
-    // Create an OpenGL context for an OpenGL window...
     SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
-
-    // ...and make it the context we are currently working in
     SDL_GL_MakeCurrent(g_display_window, context);
     gladLoadGL((GLADloadfunc)SDL_GL_GetProcAddress);
 
+    // ———————————————————————————————————————————————————————————————————————————————————
+
+    // SOFT INITIALISE ———————————————————————————————————————————————————————————————————
+    // Initialise our camera
+    glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+
+    // Load up our shaders
+    g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
+
+    // Initialise our view, model, and projection matrices
+    g_view_matrix = glm::mat4(1.0f);  // Defines the position (location and orientation) of the camera
+    g_model_matrix = glm::mat4(1.0f);  // Defines every translation, rotations, or scaling applied to an object
+    g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);  // Defines the characteristics of your camera, such as clip planes, field of view, projection method etc.
+
+    g_shader_program.set_projection_matrix(g_projection_matrix);
+    g_shader_program.set_view_matrix(g_view_matrix);
+    // Notice we haven't set our model matrix yet!
+
+    g_shader_program.set_colour(TRIANGLE_RED, TRIANGLE_BLUE, TRIANGLE_GREEN, TRIANGLE_OPACITY);
+
+    // Each object has its own unique ID
+    glUseProgram(g_shader_program.get_program_id());
+
     glClearColor(BG_RED, BG_GREEN, BG_BLUE, BG_OPACITY);
+}
 
-    /**
-     The `SDL_Event` type is a union that contains structures for the different event types, like quitting or closing.
-     */
+void process_input()
+{
     SDL_Event event;
-    while (g_game_is_running)
+    while (SDL_PollEvent(&event))
     {
-        /**
-         Basically, while OpenGL is polling, or expecting, an event from the user, don't do anything.
-         */
-        while (SDL_PollEvent(&event))
+        if (event.type == SDL_EVENT_QUIT || event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED)
         {
-            /**
-             The only types of events we are expecting thus far are simple those that end our OpenGL program.
-             This will change soon.
-             */
-            if (event.type == SDL_EVENT_QUIT || event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED)
-            {
-                g_game_is_running = false;
-            }
+            g_app_status = TERMINATED;
         }
+    }
+}
 
-        // Quite simply: clear the space in memory holding our colours
-        glClear(GL_COLOR_BUFFER_BIT);
+void update() {}
 
-        // Update a window with whatever OpenGL is rendering
-        SDL_GL_SwapWindow(g_display_window);
+void render() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    g_shader_program.set_model_matrix(g_model_matrix);
+
+    float vertices[] =
+    {
+         0.5f, -0.5f,
+         0.0f,  0.5f,
+        -0.5f, -0.5f
+    };
+
+    glVertexAttribPointer(g_shader_program.get_position_attribute(), 2, GL_FLOAT, false, 0, vertices);
+    glEnableVertexAttribArray(g_shader_program.get_position_attribute());
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDisableVertexAttribArray(g_shader_program.get_position_attribute());
+
+    SDL_GL_SwapWindow(g_display_window);
+}
+
+void shutdown() { SDL_Quit(); }
+
+/**
+ Start here—we can see the general structure of a game loop without worrying too much about the details yet.
+ */
+int main(int argc, char* argv[])
+{
+    // Initialise our program—whatever that means
+    initialise();
+
+    while (g_app_status == RUNNING)
+    {
+        process_input();  // If the player did anything—press a button, move the joystick—process it
+        update();         // Using the game's previous state, and whatever new input we have, update the game's state
+        render();         // Once updated, render those changes onto the screen
     }
 
-    SDL_Quit();
+    shutdown();  // The game is over, so let's perform any shutdown protocols
     return 0;
 }
